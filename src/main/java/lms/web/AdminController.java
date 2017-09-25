c8510536c688b0ef71ca76620cdc8fdf1c4a606e
zehan88 <814712246@qq.com>
2017-09-25 09:11:41 +0800

    adminController逻辑跳转实现

--- src/main/java/lms/web/AdminController.java
+++ src/main/java/lms/web/AdminController.java
@@ -5,6 +5,7 @@ import lms.model.QueryExample;
 import lms.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,36 +17,60 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
-    @RequestMapping("/query")
+    @RequestMapping("/queryAdmin")
     public ModelAndView query(QueryExample qe) {
         ModelAndView mv = new ModelAndView();
         List<LmsAdmin> list = adminService.findAll(qe);
-        mv.addObject("itemList", list);
-        mv.setViewName("list");
-
+        mv.addObject("admins", list);
+        mv.setViewName("admin_list");
         return mv;
     }
 
-    @RequestMapping("/login")
-    public ModelAndView login(String username, String userpassword,
-                              String inputCode, HttpSession session) {
+    @RequestMapping("/addAdmin")
+    public String addAdmin() {
+        return "redirect:/queryAdmin";
+    }
+
+    @RequestMapping("/updateAdmin")
+    public ModelAndView updateAdmin() {
         ModelAndView mv = new ModelAndView();
-//        //先验证验证码
-//        if(!session.getAttribute("numrand").equals(inputCode)){
-//            mv.addObject("msg","验证码不正确");
+        mv.setViewName("admin_edit");
+        return mv;
+    }
+    @RequestMapping("/updateAdminSub")
+    public String updateAdminSub() {
+        return "redirect:/queryAdmin";
+    }
+
+    @RequestMapping("/deleteAdmin/{id}")
+    public String deleteAdmin(@PathVariable int id) {
+//        adminService.deleteAdmin(id);
+        return "redirect:/queryAdmin";
+    }
+
+
+
+//
+//    @RequestMapping("/login")
+//    public ModelAndView login(String username, String userpassword,
+//                              String inputCode, HttpSession session) {
+//        ModelAndView mv = new ModelAndView();
+////        //先验证验证码
+////        if(!session.getAttribute("numrand").equals(inputCode)){
+////            mv.addObject("msg","验证码不正确");
+////            mv.setViewName("login");
+////            return mv;
+////        }
+//
+//        LmsAdmin loginUser = adminService.login(username, userpassword);
+//        if(loginUser != null){
+//            session.setAttribute("user", loginUser);
+//            mv.setViewName("redirect:/index");
+//            return mv;
+//        } else {
+//            mv.addObject("msg","用户名或密码错误");
 //            mv.setViewName("login");
 //            return mv;
 //        }
-
-        LmsAdmin loginUser = adminService.login(username, userpassword);
-        if(loginUser != null){
-            session.setAttribute("user", loginUser);
-            mv.setViewName("redirect:/index");
-            return mv;
-        } else {
-            mv.addObject("msg","用户名或密码错误");
-            mv.setViewName("login");
-            return mv;
-        }
-    }
+//    }
 }
